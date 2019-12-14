3ce91b1659f70b2c88b4cefb40ae64b056b6c3f6
bertreb <gitbertreb@gmail.com>
2019-12-14 19:37:41 +0100

    git prep

--- alarm-clock.js
+++ alarm-clock.js
@@ -3,30 +3,30 @@
 command = process.argv[2];
 require('console-stamp')(console);
 if(!command || command === "start") {
-  require('./coffee-cache.js').setCacheDir('.jscache/');
+  require('./node_modules/coffee-cache/lib/coffee-cache.js').setCacheDir('.jscache/');
   console.log('Alarm Clock starting...')
-  
+
   var AlarmClock = require('./alarm-clock.coffee');
   var app = new AlarmClock();
   var exitCode = 0;
-  
+
   terminate = function() {
     console.log('Shutting down alarm-clock...');
     app.destroy().then( () => {
       console.log('Stopped.');
       process.exit(exitCode);
-      
+
     }).catch( (error) => {
       console.error(error);
       exitCode = 1;
       process.exit(exitCode);
     })
-    
+
     //}).finally( () => {
     //  process.exit(exitCode);
     //});
   }
   process.on('SIGINT', terminate)
   process.on('SIGTERM', terminate)
-  
+
 }
